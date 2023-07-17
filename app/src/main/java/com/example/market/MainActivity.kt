e6d4e170aa99d3b3936fc7dc7ab4009aa754f066
Mina Ezzat <53598947+MeNa619493@users.noreply.github.com>
2023-07-17 19:59:11 +0300

    ui categories

--- app/src/main/java/com/example/market/MainActivity.kt
+++ app/src/main/java/com/example/market/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.market
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.market.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(binding.navigationBar, navController)
     }
 }
