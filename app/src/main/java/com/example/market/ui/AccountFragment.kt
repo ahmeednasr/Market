67d318bd7f93fc1c0518b273011383d39307f3df
mdanash97 <m.elamirdanash@gmail.com>
2023-07-17 23:03:15 +0300

    authentications and dagger-hilt dependencies

--- app/src/main/java/com/example/market/ui/AccountFragment.kt
+++ app/src/main/java/com/example/market/ui/AccountFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.market.R
 import com.example.market.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
@@ -18,8 +20,16 @@ class AccountFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_accountFragment_to_authIntro)
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
