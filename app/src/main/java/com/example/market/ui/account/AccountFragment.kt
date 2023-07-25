2e85ee03c6c1017402528552712bfa22127d6847
ahmeednasr <ahmedna7sr@gmail.com>
2023-07-25 10:36:46 +0300

    add local

--- app/src/main/java/com/example/market/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/market/ui/account/AccountFragment.kt
@@ -182,9 +182,7 @@ class AccountFragment : Fragment() {
         //val currencyFrom=
         editor.apply()
         binding.currencyValue.text = selectedItem
-        viewModel.convertCurrency("EGP", selectedItem!!, 1.0)
         Toast.makeText(requireContext(), selectedItem, Toast.LENGTH_SHORT).show()
-
     }
 
     private fun showDialog() {
